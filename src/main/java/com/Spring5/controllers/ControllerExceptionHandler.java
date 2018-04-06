882e9dc1053631eb405e346d9499be845fa7dd85
Annie Fisher <annielynn.fisher@gmail.com>
2018-04-06 17:20:46 -0600

    refactored exception handler controller to work

--- src/main/java/com/Spring5/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/Spring5/controllers/ControllerExceptionHandler.java
@@ -1,25 +1,27 @@
 package com.Spring5.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormat(Exception e){
-//
-//        log.error("Handling Number Format Exception");
-//        log.error(e.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("400error");
-//        modelAndView.addObject("exception", e);
-//
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormat(Exception e, Model model){
+
+        log.error("Handling binding exception");
+        log.error(e.getMessage());
+
+       model.addAttribute("exception", e);
+
+        return "400error";
+    }
 }
 
